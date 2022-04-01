747f11ecb7029414ef971847ab1eec62cd077e5d
vaishnavi0198 <vaishnavikaushik00@gmail.com>
2022-04-01 13:19:37 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Navbar from "./components/Navbar";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Home from './components/Pages/Home';
+import Services from './components/Pages/Services';
+import Products from './components/Pages/Products';
+import SignUp from './components/Pages/SignUp';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      
+      <BrowserRouter> 
+      <Navbar />
+        <Routes>
+         <Route path="/" element={<Home />} exact>
+              </Route>
+         <Route path="/services" element={<Services />} >
+              </Route>
+         <Route path="/products" element={<Products />} >
+              </Route>
+         <Route path="/sign-up" element={<SignUp />} >
+              </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
 
 export default App;
+
+
+// path issue - routing (services, home , products etc)
+// second card dimensions 
---
+++ src/components/Pages/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../../App.css';
+import Card from '../Card';
+import HeroSection from '../HeroSection';
+import Footer from '../Footer';
+
+function Home() {
+    return(
+        <>
+        <HeroSection />
+        <Card />
+        <Footer />
+        </>
+    )
+}
+
+export default Home;
